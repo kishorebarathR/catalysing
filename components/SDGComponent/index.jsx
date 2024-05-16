d729644b1ff5879d4b40ab7e671c6148aa7ff2cd
kishorebarath011099@gmail.com <kishorebarath011099@gmail.com>
2024-05-16 16:33:23 +0530

    sdg components changes

--- components/SDGComponent/index.jsx
+++ components/SDGComponent/index.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import Header from "../Shared/Header"
 import Footer from "../Shared/Footer"
-import SDGpage from "./Sdgpage"
-import SDG02page from "./Sdg02page"
+import Sdgpage from "./Sdgpage"
+import Sdg02page from "./Sdg02page"
 import Seo from "../SeoComponents/Seo"
 
 const index = () => {
   const title = "SDG"
-  const description =" Arrive at stakeholder consensus on how to achieve SDG 2030targets through emulating good practices Entrepreneur network with significant reach and impact and  capability to accelerate SDGs through social"
+  const description =
+    " Arrive at stakeholder consensus on how to achieve SDG 2030targets through emulating good practices Entrepreneur network with significant reach and impact and  capability to accelerate SDGs through social"
   const path = "https://catalysingsocialimpact.in/sdg.html"
   const metaImage = ""
   return (
@@ -18,9 +19,8 @@ const index = () => {
         path={path}
         metaImage={metaImage}
       />
-      <SDGpage />
-      <SDG02page />
-      
+      <Sdgpage />
+      <Sdg02page />
     </>
   )
 }
