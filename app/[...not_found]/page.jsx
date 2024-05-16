a4e1651026faa5b6912d3d7a2ad883c9d16e772e
kishorebarath011099@gmail.com <kishorebarath011099@gmail.com>
2024-05-16 16:21:28 +0530

    page 404/[...not_found] is updated

---
+++ app/[...not_found]/page.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const page = () => {
+  return (
+    <>
+      <div className="h-screen w-full flex flex-col justify-center items-center">
+        <div>
+          <p className="text-3xl font-semibold">Page Not Found</p>
+          <p className="mt-3 text-xl">
+            Apologies, but we were unable to find what you were looking for.
+          </p>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default page
---
+++ app/sdg/page.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const page = () => {
+  return <div>This is SDG PropTypes.resetWarningCache()</div>
+}
+
+export default page
